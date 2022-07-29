8c94fcfd7c97b5cd7ce1c0693723b2ba5c9f840a
준 강 <junijuniya@naver.com>
2022-07-29 21:38:30 +0900

    Maui.LottieTest 추가

---
+++ Maui.LottieTest/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.LottieTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Maui.LottieTest/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace Maui.LottieTest
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseSkiaSharp() // 추가
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            return builder.Build();
+        }
+    }
+}
