2306d1ed6dcf94dbe2b5875f494d23403405ef2b
준 강 <junijuniya@naver.com>
2022-07-31 23:19:51 +0900

    Maui.BlazorAppTest  추가

---
+++ Maui.BlazorAppTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui.BlazorAppTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
