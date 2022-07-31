9c61b5c8a45df6836bce1971d11c9f1d2eb03a07
준 강 <junijuniya@naver.com>
2022-07-31 23:11:37 +0900

    Maui.AdmobTest 추가

---
+++ Maui.AdmobTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui.AdmobTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui.AdmobTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.AdmobTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
