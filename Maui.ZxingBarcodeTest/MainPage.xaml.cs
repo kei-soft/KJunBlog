f42fc997225a20ed0958f8eed3b25d208506f619
준 강 <junijuniya@naver.com>
2022-08-19 21:11:05 +0900

    Maui.ZxingBarcodeTest 추가

---
+++ Maui.ZxingBarcodeTest/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Maui.ZxingBarcodeTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BarcodeReader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+        {
+            Dispatcher.Dispatch(() =>
+            {
+                this.barcodeResult.Text = $"{e.Results[0].Value}{Environment.NewLine}[{e.Results[0].Format}]";
+
+                //this.generateBarcode.Format = e.Results[0].Format; // Error 발생
+                this.generateBarcode.Value = e.Results[0].Value;
+            });
+        }
+
+        private void touchOnSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            this.barcodeReader.IsTorchOn = this.touchOnSwitch.IsToggled;
+        }
+
+        private void cameraSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (this.cameraSwitch.IsToggled)
+            {
+                this.barcodeReader.CameraLocation = ZXing.Net.Maui.CameraLocation.Front;
+            }
+            else
+            {
+                this.barcodeReader.CameraLocation = ZXing.Net.Maui.CameraLocation.Rear;
+            }
+        }
+    }
+}
---
+++ Maui.ZxingBarcodeTest/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using ZXing.Net.Maui;
+
+namespace Maui.ZxingBarcodeTest
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseBarcodeReader()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+            .ConfigureMauiHandlers(h =>
+             {
+                 h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
+                 h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraView), typeof(CameraViewHandler));
+                 h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
+             });
+
+            return builder.Build();
+        }
+    }
+}
