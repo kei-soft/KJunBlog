2d9af78cec285065b8ea4e944f85b46edc0d09c3
준 강 <junijuniya@naver.com>
2022-08-19 22:04:46 +0900

    .

--- Maui.ZxingBarcodeTest/MauiProgram.cs
+++ Maui.ZxingBarcodeTest/MauiProgram.cs
@@ -9,18 +9,19 @@ namespace Maui.ZxingBarcodeTest
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .UseBarcodeReader()
+                .UseBarcodeReader() // 추가
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 })
-            .ConfigureMauiHandlers(h =>
-             {
-                 h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
-                 h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraView), typeof(CameraViewHandler));
-                 h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
-             });
+                // 추가
+                .ConfigureMauiHandlers(h =>
+                 {
+                     h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
+                     h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraView), typeof(CameraViewHandler));
+                     h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
+                 });
 
             return builder.Build();
         }
