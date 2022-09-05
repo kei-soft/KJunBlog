3a26c15788cea233ab8c32ef361088f3c4904a2e
준 강 <junijuniya@naver.com>
2022-09-05 23:57:29 +0900

    Maui.MapTest 추가

---
+++ Maui.MapTest/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui.MapTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //Register Syncfusion license - 주석을 풀어 키 입력
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("부여받은라이센스키");
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Maui.MapTest/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Maui.MapTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void NavigateToMap()
+        {
+            var location = new Location(37.442018, 126.996245);
+            var options = new MapLaunchOptions { Name = "관문체육공원" };
+
+            try
+            {
+                await Map.Default.OpenAsync(location, options);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public async void DriveToMap()
+        {
+            var location = new Location(37.442018, 126.996245);
+            var options = new MapLaunchOptions
+            {
+                Name = "관문체육공원",
+                NavigationMode = NavigationMode.Driving
+            };
+
+            try
+            {
+                await Map.Default.OpenAsync(location, options);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void LocationButton_Clicked(object sender, EventArgs e)
+        {
+            NavigateToMap();
+        }
+
+        private void DriveButton_Clicked(object sender, EventArgs e)
+        {
+            DriveToMap();
+        }
+
+    }
+}
