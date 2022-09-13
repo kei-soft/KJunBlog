61e148ff4adcb6ee73bceef9a66a5b264bf89c2b
준 강 <junijuniya@naver.com>
2022-09-14 00:29:40 +0900

    Maui.ToolKitMaui 추가

---
+++ Maui.ToolKitMaui/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+using Font = Microsoft.Maui.Font;
+
+namespace Maui.ToolKitMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void snackbarButton_Clicked(object sender, EventArgs e)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            var snackbarOptions = new SnackbarOptions
+            {
+                BackgroundColor = Colors.Black,
+                TextColor = Colors.White,
+                ActionButtonTextColor = Colors.Yellow,
+                CornerRadius = new CornerRadius(10),
+                Font = Font.SystemFontOfSize(14),
+                ActionButtonFont = Font.SystemFontOfSize(14),
+                CharacterSpacing = 0.5,
+            };
+
+            string text = "Show Snackbar!!";
+            string actionButtonText = "Click Here Action Execute";
+            Action action = async () => await DisplayAlert("Snackbar ActionButton", "Snackbar ActionButton  Click", "OK");
+            TimeSpan duration = TimeSpan.FromSeconds(3);
+
+            var snackbar = Snackbar.Make(text, action, actionButtonText, duration, snackbarOptions);
+
+            snackbar.Show(cancellationTokenSource.Token);
+        }
+    }
+}
