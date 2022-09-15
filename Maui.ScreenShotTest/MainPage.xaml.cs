9f906e1d5efa199b51bd1febd9c5babb93a3a02f
준 강 <junijuniya@naver.com>
2022-09-15 10:58:34 +0900

    Maui.ScreenShotTest  추가

---
+++ Maui.ScreenShotTest/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Maui.ScreenShotTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnScreenShot(object sender, EventArgs e)
+        {
+            this.image.Source = await TakeScreenshotAsync();
+        }
+
+        public async Task<ImageSource> TakeScreenshotAsync()
+        {
+            if (Screenshot.Default.IsCaptureSupported)
+            {
+                IScreenshotResult screenShotResult = await Screenshot.Default.CaptureAsync();
+                Stream stream = await screenShotResult.OpenReadAsync();
+
+                return ImageSource.FromStream(() => stream);
+            }
+
+            return null;
+        }
+    }
+}
