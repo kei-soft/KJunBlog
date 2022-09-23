6f3711d54ee33c2745e224503417afb25d6022b0
kjun <keisoft.kjun@gmail.com>
2022-09-23 16:20:58 +0900

    AnimationBehavior

---
+++ Maui.ToolKitMaui/Animations/SampleScaleAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Maui.Animations;
+
+namespace Maui.ToolKitMaui.Animations
+{
+    class SampleScaleAnimation : BaseAnimation
+    {
+        public override async Task Animate(VisualElement view)
+        {
+            await view.ScaleTo(1.2, Length, Easing);
+            await view.ScaleTo(1, Length, Easing);
+        }
+    }
+
+    class SampleScaleToAnimation : BaseAnimation
+    {
+        public double Scale { get; set; }
+
+        public override Task Animate(VisualElement view) => view.ScaleTo(Scale, Length, Easing);
+    }
+}
