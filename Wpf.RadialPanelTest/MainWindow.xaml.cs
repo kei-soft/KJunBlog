d789c5b46047c946c1562de1740a8146785e9874
준 강 <junijuniya@naver.com>
2022-12-13 23:20:33 +0900

    Wpf.RadialPanelTest 추가

---
+++ Wpf.RadialPanelTest/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf.RadialPanelTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+                btn.Content = "Button " + (i + 1);
+                byHeightRadialPanel.Children.Add(btn);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+                btn.Content = "Button " + (i + 1);
+                byWidthRadialPanel.Children.Add(btn);
+            }
+        }
+    }
+}
